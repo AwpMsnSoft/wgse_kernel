36fca219a72970cee587a351109ea4a20050efda
MiracleSNeko <miracle.neko@qq.com>
2023-11-13 07:45:32 +0000

    dev(errors): use `thiserror` crate to generate error types

--- src/errors.rs
+++ src/errors.rs
@@ -1,45 +1,8 @@
-use std::{
-    error::Error,
-    fmt::{Display, Formatter, Result},
-};
+use thiserror::Error;
+type Message = &'static str;
 
-#[derive(Debug, Clone)]
-pub struct InconsistentTypeError {
-    caller: String,
-    expect: String,
-    found: String,
+#[derive(Clone, Debug, Error)]
+pub enum WgseUtilsError {
+    #[error("inconsistent type, expect `{0}`, found `{1}`")]
+    InconsistentType(Message, Message),
 }
-
-impl InconsistentTypeError {
-    pub fn new<T>(caller: T, expect: T, found: T) -> Self
-    where
-        T: ToString,
-    {
-        Self {
-            caller: caller.to_string(),
-            expect: expect.to_string(),
-            found: found.to_string(),
-        }
-    }
-}
-
-impl Display for InconsistentTypeError {
-    fn fmt(&self, fmt: &mut Formatter<'_>) -> Result {
-        write!(
-            fmt,
-            "inconsistent type error raised while calling `{}`. expect {}, found {}",
-            self.caller, self.expect, self.found
-        )
-    }
-}
-
-impl Error for InconsistentTypeError {}
-
-#[macro_export]
-macro_rules! inconsistent_type_err {
-    ($exp: expr, $fnd: expr) => {{
-        InconsistentTypeError::new(function_name!(), $exp, $fnd).into()
-    }};
-}
-
-pub use inconsistent_type_err;
